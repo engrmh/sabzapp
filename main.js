b20c1db5c3b18de45543bb268dee235b7116e6cb
GALAHAD <59095451+engrmh@users.noreply.github.com>
2024-05-27 01:43:31 +0330

    feat: prevent record and screen capture

--- main.js
+++ main.js
@@ -1,29 +1,35 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('node:path')
+const { app, BrowserWindow, globalShortcut } = require("electron");
+const path = require("node:path");
 
-function createWindow () {
+function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, "preload.js"),
     },
-    icon: path.join(__dirname, 'icon', 'mac.icns')
-  })
+    icon: path.join(__dirname, "icon", "mac.icns"),
+  });
 
-  mainWindow.loadURL('https://sabzlearn.ir/')
+  mainWindow.loadURL("https://sabzlearn.ir/");
 
+  mainWindow.webContents.on("devtools-opened", () => {
+    mainWindow.webContents.closeDevTools();
+  });
+
+  globalShortcut.register("PrintScreen", () => {
+    mainWindow.close();
+  });
 }
 
 app.whenReady().then(() => {
-  createWindow()
+  createWindow();
 
-  app.on('activate', function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+  app.on("activate", function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
-
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
